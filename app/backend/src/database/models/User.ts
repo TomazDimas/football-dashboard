4c71eaf55a839373afd015b7fd42a8d86650fc8e
TomazDimas <tomazdimas2001@gmail.com>
2022-10-21 20:17:19 -0300

    Criação

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,39 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id: number;
+  name: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(30),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(30),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
